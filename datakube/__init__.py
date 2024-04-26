fe3deb9ccd980a7c4916c70d20bc4e73ed271675
David Morrison <drmorr@appliedcomputing.io>
2024-04-26 16:27:12 -0700

    add to __init__.py

--- datakube/__init__.py
+++ datakube/__init__.py
@@ -1,3 +1,11 @@
-import pandas as pd
+from .df_utils import partition_and_normalize
+from .df_utils import sanitize
+from .k8s_utils import fetch_pod_intervals
+from .k8s_utils import read_obj_from_json
 
-pd.options.mode.copy_on_write = True
+__all__ = [
+    "fetch_pod_intervals",
+    "read_obj_from_json",
+    "sanitize",
+    "partition_and_normalize",
+]
