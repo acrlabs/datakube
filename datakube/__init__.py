bf885a0827a6fedf4e0eaf33d7ff765ebda5f017
David Morrison <drmorr@appliedcomputing.io>
2024-05-03 21:16:30 -0700

    cache S3 files locally

--- datakube/__init__.py
+++ datakube/__init__.py
@@ -4,20 +4,24 @@ from bokeh.io import output_notebook
 pd.options.mode.copy_on_write = True
 output_notebook()
 
+from .df_utils import aggregate_timeseries
 from .df_utils import extract_labels_to_columns
 from .df_utils import partition_and_normalize
 from .df_utils import sanitize
 from .k8s_utils import fetch_pod_intervals
 from .k8s_utils import read_obj_from_json
 from .plot_utils import new_figure
-from .prom_utils import compute_pod_owners_map
+from .plot_utils import plot_multiseries
+from .prom_utils import PromReader
 
 __all__ = [
-    "compute_pod_owners_map",
+    "aggregate_timeseries",
     "extract_labels_to_columns",
     "fetch_pod_intervals",
     "new_figure",
     "read_obj_from_json",
     "sanitize",
     "partition_and_normalize",
+    "plot_multiseries",
+    "PromReader",
 ]
--- datakube/prom_utils.py
+++ datakube/prom_utils.py
@@ -1,43 +1,78 @@
+import os
+import re
+
+import boto3
 import pandas as pd
 
 from datakube.df_utils import extract_labels_to_columns
 
 
-def compute_pod_owners_map(path: str) -> pd.DataFrame:
-    pod_owners = pd.read_parquet(f"{path}/kube_pod_owner")
-    pod_owners = extract_labels_to_columns(pod_owners, ["owner_name", "owner_kind"])
-    pod_owners.drop_duplicates(subset="pod", inplace=True, ignore_index=True)
+class PromReader:
+    def __init__(self, data_path: str, cache_root: str = "~/.cache/datakube") -> None:
+        self.cache_root = os.path.expanduser(cache_root)
+        self.s3 = boto3.client("s3")
+        self.cache_enabled = True
+        self.data_path = data_path
 
-    rs_owners = pd.read_parquet(f"{path}/kube_replicaset_owner")
-    rs_owners = extract_labels_to_columns(rs_owners, ["replicaset", "owner_name"])
-    rs_owners.drop_duplicates(subset="replicaset", inplace=True, ignore_index=True)
+    def metric_to_df(self, metric_name: str) -> pd.DataFrame:
+        path = f"{self.data_path}/{metric_name}"
+        path_parts = re.match(r"s3://([a-zA-Z_-]+)/(.*)", path)
+        if path_parts and self.cache_enabled:
+            bucket = path_parts.group(1)
+            prefix = path_parts.group(2)
+            cached_location = f"{self.cache_root}/{bucket}"
 
-    job_owners = pd.read_parquet(f"{path}/kube_job_owner")
-    job_owners = extract_labels_to_columns(job_owners, ["job_name", "owner_name"])
-    job_owners.drop_duplicates(subset="job_name", inplace=True, ignore_index=True)
+            paginator = self.s3.get_paginator("list_objects_v2")
+            for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
+                for obj in page["Contents"]:
+                    full_path = f"{cached_location}/{obj['Key']}"
+                    if not os.path.exists(full_path):
+                        d = os.path.dirname(obj["Key"])
+                        os.makedirs(f"{cached_location}/{d}", exist_ok=True)
 
-    pod_owners = pod_owners.merge(
-        rs_owners,
-        how="left",
-        left_on=["owner_name", "namespace"],
-        right_on=["replicaset", "namespace"],
-        suffixes=(None, "_rs"),
-    )
+                        resp = self.s3.get_object(Bucket=bucket, Key=obj["Key"])
+                        with open(full_path, "wb") as f:
+                            f.write(resp["Body"].read())
 
-    pod_owners = pod_owners.merge(
-        job_owners,
-        how="left",
-        left_on=["owner_name", "namespace"],
-        right_on=["job_name", "namespace"],
-        suffixes=(None, "_job"),
-    )
+            return pd.read_parquet(f"{cached_location}/{prefix}")
 
-    # The only owner metrics exposed by kube-state-metrics are pod, job, and rs:
-    # it's possible other things also have nested ownership, but we're not worried
-    # about those right now -- the standard k8s things (StatefulSets, DaemonSets,
-    # static pods) have fixed owners so we just copy those values over
-    pod_owners["root_owner"] = (
-        pod_owners["owner_name_rs"].fillna(pod_owners["owner_name_job"]).fillna(pod_owners["owner_name"])
-    )
+        return pd.read_parquet(path)
 
-    return pod_owners[["pod", "namespace", "root_owner"]]
+    def compute_pod_owners_map(self) -> pd.DataFrame:
+        pod_owners = self.metric_to_df("kube_pod_owner")
+        pod_owners = extract_labels_to_columns(pod_owners, ["owner_name", "owner_kind"])
+        pod_owners.drop_duplicates(subset="pod", inplace=True, ignore_index=True)
+
+        rs_owners = self.metric_to_df("kube_replicaset_owner")
+        rs_owners = extract_labels_to_columns(rs_owners, ["replicaset", "owner_name"])
+        rs_owners.drop_duplicates(subset="replicaset", inplace=True, ignore_index=True)
+
+        job_owners = self.metric_to_df("kube_job_owner")
+        job_owners = extract_labels_to_columns(job_owners, ["job_name", "owner_name"])
+        job_owners.drop_duplicates(subset="job_name", inplace=True, ignore_index=True)
+
+        pod_owners = pod_owners.merge(
+            rs_owners,
+            how="left",
+            left_on=["owner_name", "namespace"],
+            right_on=["replicaset", "namespace"],
+            suffixes=(None, "_rs"),
+        )
+
+        pod_owners = pod_owners.merge(
+            job_owners,
+            how="left",
+            left_on=["owner_name", "namespace"],
+            right_on=["job_name", "namespace"],
+            suffixes=(None, "_job"),
+        )
+
+        # The only owner metrics exposed by kube-state-metrics are pod, job, and rs:
+        # it's possible other things also have nested ownership, but we're not worried
+        # about those right now -- the standard k8s things (StatefulSets, DaemonSets,
+        # static pods) have fixed owners so we just copy those values over
+        pod_owners["root_owner"] = (
+            pod_owners["owner_name_rs"].fillna(pod_owners["owner_name_job"]).fillna(pod_owners["owner_name"])
+        )
+
+        return pod_owners[["pod", "namespace", "root_owner"]]
